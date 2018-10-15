ed27be977548579f14c69625e95a44b95e910602
Alexey <medvedev.lasta@yandex.ru>
2018-10-16 01:54:05 +0700

    Способ ввода имени входного файла изменён на stdin

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func checkErr(e error)  {
+func checkErr(e error) {
 	if e != nil {
 		fmt.Println("Error opening file")
 		os.Exit(1)
@@ -15,7 +15,7 @@ func checkErr(e error)  {
 }
 
 func sort(result []int) {
-	for j :=0; j < len(result)-1; j++  {
+	for j := 0; j < len(result)-1; j++ {
 		for i := 0; i < len(result)-1-j; i++ {
 			if result[i] > result[i+1] {
 				tmp := result[i]
@@ -28,23 +28,16 @@ func sort(result []int) {
 
 func main() {
 
-	if len(os.Args) > 2  {
-		fmt.Println("Too many arguments")
-		os.Exit(1)
-	} else if len(os.Args) < 2 {
-		fmt.Println("Not enough argumetns")
-		os.Exit(1)
-	}
-
-	fileName := os.Args[1]
+	var fileName string
+	fmt.Scan(&fileName)
 	file, err := ioutil.ReadFile(fileName)
 	checkErr(err)
 
-	var result []int;
+	var result []int
 	fileText := string(file)
-	for _,letter := range fileText  {
-		if string(letter) != " "  {
-			if number,err :=strconv.Atoi(string(letter)); err == nil {
+	for _, letter := range fileText {
+		if string(letter) != " " {
+			if number, err := strconv.Atoi(string(letter)); err == nil {
 				result = append(result, number)
 			} else {
 				fmt.Println("The file contains a foreign character:", string(letter))
@@ -55,7 +48,7 @@ func main() {
 
 	sort(result)
 
-	for _,number := range result {
+	for _, number := range result {
 		fmt.Print(number, " ")
 	}
 }
